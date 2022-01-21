abc0d563ae455fc15f880f7b5d3be085330bec11
Oussama Rahmouni <ousrah@hotmail.com>
2022-01-21 17:48:14 +0100

    Ajoutez des fichiers projet.

---
+++ ModeDeconnecte202/FrmMedecin.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace ModeDeconnecte202
+{
+    partial class FrmMedecin
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMedecins = new System.Windows.Forms.ListBox();
+            this.txtnom = new System.Windows.Forms.TextBox();
+            this.txtprenom = new System.Windows.Forms.TextBox();
+            this.txtChercher = new System.Windows.Forms.TextBox();
+            this.btnChercher = new System.Windows.Forms.Button();
+            this.btnPrecedent = new System.Windows.Forms.Button();
+            this.btnSuivant = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstMedecins
+            // 
+            this.lstMedecins.FormattingEnabled = true;
+            this.lstMedecins.ItemHeight = 22;
+            this.lstMedecins.Location = new System.Drawing.Point(36, 80);
+            this.lstMedecins.Margin = new System.Windows.Forms.Padding(4);
+            this.lstMedecins.Name = "lstMedecins";
+            this.lstMedecins.Size = new System.Drawing.Size(330, 246);
+            this.lstMedecins.TabIndex = 0;
+            // 
+            // txtnom
+            // 
+            this.txtnom.Location = new System.Drawing.Point(580, 112);
+            this.txtnom.Name = "txtnom";
+            this.txtnom.Size = new System.Drawing.Size(182, 28);
+            this.txtnom.TabIndex = 1;
+            // 
+            // txtprenom
+            // 
+            this.txtprenom.Location = new System.Drawing.Point(580, 147);
+            this.txtprenom.Name = "txtprenom";
+            this.txtprenom.Size = new System.Drawing.Size(182, 28);
+            this.txtprenom.TabIndex = 2;
+            // 
+            // txtChercher
+            // 
+            this.txtChercher.Location = new System.Drawing.Point(36, 36);
+            this.txtChercher.Name = "txtChercher";
+            this.txtChercher.Size = new System.Drawing.Size(222, 28);
+            this.txtChercher.TabIndex = 3;
+            // 
+            // btnChercher
+            // 
+            this.btnChercher.Location = new System.Drawing.Point(264, 33);
+            this.btnChercher.Name = "btnChercher";
+            this.btnChercher.Size = new System.Drawing.Size(102, 34);
+            this.btnChercher.TabIndex = 4;
+            this.btnChercher.Text = "Chercher";
+            this.btnChercher.UseVisualStyleBackColor = true;
+            this.btnChercher.Click += new System.EventHandler(this.btnChercher_Click);
+            // 
+            // btnPrecedent
+            // 
+            this.btnPrecedent.Location = new System.Drawing.Point(88, 334);
+            this.btnPrecedent.Name = "btnPrecedent";
+            this.btnPrecedent.Size = new System.Drawing.Size(75, 35);
+            this.btnPrecedent.TabIndex = 5;
+            this.btnPrecedent.Text = "<";
+            this.btnPrecedent.UseVisualStyleBackColor = true;
+            this.btnPrecedent.Click += new System.EventHandler(this.btnPrecedent_Click);
+            // 
+            // btnSuivant
+            // 
+            this.btnSuivant.Location = new System.Drawing.Point(227, 333);
+            this.btnSuivant.Name = "btnSuivant";
+            this.btnSuivant.Size = new System.Drawing.Size(75, 35);
+            this.btnSuivant.TabIndex = 6;
+            this.btnSuivant.Text = ">";
+            this.btnSuivant.UseVisualStyleBackColor = true;
+            this.btnSuivant.Click += new System.EventHandler(this.btnSuivant_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(413, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nom :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(413, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Prénom :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(413, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Code :";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(576, 80);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(0, 24);
+            this.lblId.TabIndex = 10;
+            // 
+            // FrmMedecin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 405);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSuivant);
+            this.Controls.Add(this.btnPrecedent);
+            this.Controls.Add(this.btnChercher);
+            this.Controls.Add(this.txtChercher);
+            this.Controls.Add(this.txtprenom);
+            this.Controls.Add(this.txtnom);
+            this.Controls.Add(this.lstMedecins);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmMedecin";
+            this.Text = "Gestion des medecins";
+            this.Load += new System.EventHandler(this.FrmMedecin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstMedecins;
+        private System.Windows.Forms.TextBox txtnom;
+        private System.Windows.Forms.TextBox txtprenom;
+        private System.Windows.Forms.TextBox txtChercher;
+        private System.Windows.Forms.Button btnChercher;
+        private System.Windows.Forms.Button btnPrecedent;
+        private System.Windows.Forms.Button btnSuivant;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblId;
+    }
+}
+
---
+++ ModeDeconnecte202/FrmMedecin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace ModeDeconnecte202
+{
+    public partial class FrmMedecin : Form
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        DataSet ds = new DataSet();
+        SqlDataAdapter da;
+        BindingSource bs = new BindingSource();
+
+        public FrmMedecin()
+        {
+            InitializeComponent();
+        }
+        private void FrmMedecin_Load(object sender, EventArgs e)
+        {
+            //configuration de la chaine de connection
+            //string cs = @"data source=.\sqlexpress2008;initial catalog=CabinetMedecin;user id=sa;password=P@ssw0rd";
+            //string cs = @"data source=.\sqlexpress2008;initial catalog=CabinetMedecin;integrated security=true";
+            string cs = ConfigurationManager.ConnectionStrings["cabinetMedecinConnectionString"].ConnectionString;
+            cn.ConnectionString = cs;
+            cn.Open();
+
+            com.Connection = cn;
+            com.CommandText = "select * from medecin";
+
+            //remplissage de la data set
+            da = new SqlDataAdapter(com);
+            da.Fill(ds, "medecin");
+       
+            //configuration de la binding source
+            bs.DataSource = ds;
+            bs.DataMember = "medecin";
+
+
+            //affichage de la liste box
+            lstMedecins.DisplayMember = "nom";
+            lstMedecins.ValueMember = "id";
+            lstMedecins.DataSource = bs;
+
+            //affichage des zones de texte
+            lblId.DataBindings.Add("Text", bs, "id");
+            txtnom.DataBindings.Add("Text", bs, "nom");
+            txtnom.DataBindings.Add("Tag", bs, "id");
+
+            txtprenom.DataBindings.Add("Text", bs, "prenom");
+
+
+            //  MessageBox.Show("ça marche");
+
+
+        }
+
+        private void btnSuivant_Click(object sender, EventArgs e)
+        {
+            bs.MoveNext();
+        }
+
+        private void btnPrecedent_Click(object sender, EventArgs e)
+        {
+            bs.MovePrevious();
+        }
+
+        private void btnChercher_Click(object sender, EventArgs e)
+        {
+            bs.Filter = "nom like '%" + txtChercher.Text + "%'";
+        }
+    }
+}
